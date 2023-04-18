e67950634ee5e99822e9a0fdc3691d04bc6fd600
maharrambayramov <meherrem.bayramov.03@gmail.com>
2023-04-19 01:14:30 +0400

    task 2

--- js intro task/app.js
+++ js intro task/app.js
@@ -1,11 +1,29 @@
-let num = 548;
-for (i = 0; i <= num; i++) {
+// let num = 548;
+// for (i = 0; i <= num; i++) {
 
-    if (num % 2 == 0) {
-        num /= 2
+//     if (num % 2 == 0) {
+//         num /= 2
+//     }
+//     else if (num % 2 != 0) {
+//         num += 1
+//     }
+//     console.log(num);
+// }]
+let num = 584;
+function sum(num) {
+    let a = Math.floor(num / 100);
+    let b = Math.floor((num % 100) / 10);
+    let c = num % 10;
+    let max = num;
+    if (b * 100 + a * 10 + c > max) {
+        max = b * 100 + a * 10 + c;
     }
-    else if (num % 2 != 0) {
-        num += 1
+    if (c * 100 + b * 10 + a > max) {
+        max = c * 100 + b * 10 + a;
     }
-    console.log(num);
+    if (c * 100 + a * 10 + b > max) {
+        max = c * 100 + a * 10 + b;
+    }
+    return max *2;
 }
+console.log(sum(num));
